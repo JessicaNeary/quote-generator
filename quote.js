314fe4aedae995fddb775b4c6e54036f9a05f331
Jess Neary <Caneary@hotmail.com>
2016-11-12 13:22:40 +1300

    Added tweet button

--- quote.js
+++ quote.js
@@ -1,20 +1,21 @@
-var quotes = ["quote1", "quote2", "quote3"]
+var quote = '';
+var author = '';
 
 function requestQuote() {
-	console.log("Request quote")
 
 	var xhr = new XMLHttpRequest()
 
 	xhr.onreadystatechange = function() {
-	    if (xhr.readyState == XMLHttpRequest.DONE) {
-	        var json = JSON.parse(xhr.responseText);
-					var quote = json["quote"];
-					var author = json["author"];
-					console.log(quote)
-	    }
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      var json = JSON.parse(xhr.responseText);
+			quote = json["quote"];
+			author = json["author"];
+			document.getElementById("quote-display").innerHTML = quote;
+			document.getElementById("author-display").innerHTML = author;
+    }
 	}
 
-	xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies", false)
+	xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies", true)
 
 	xhr.setRequestHeader("X-Mashape-Key", "D50aO827kvmsh33DHgK3KLy0gOaOp15DkpKjsn7FAZdKRdSOxL")
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
@@ -23,17 +24,11 @@ function requestQuote() {
 	xhr.send()
 }
 
-function pickQuote(quotes){
-	return quotes[Math.floor(Math.random() * quotes.length)];
+function openURL(url) {
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 
-function showQuote() {
-	console.log("Show quote")
+function tweet(){
 
-	var quote = pickQuote(quotes);
-
-	console.log(quote);
-
-	document.getElementById("quote-display").innerHTML = quote;
-
+ openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));
 }
